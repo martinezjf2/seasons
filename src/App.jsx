bccd64338d8c558b91ea52a89d8321c57d28291b
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-23 00:16:23 -0500

    get current users position location

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
 function App() {
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+    
     return (
         <div>
             Hello World!
