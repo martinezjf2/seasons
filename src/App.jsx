53e572eb5841ca6dc9da420be315263c9a8c0ff4
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-23 00:36:32 -0500

    created a App class component instead of a functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import SeasonDisplay from "./SeasonDisplay";
 
-function App() {
+// function App() {
+//     return (
+//         <div>
+//             Hello World!
+//             <SeasonDisplay />
+//         </div>
+//     )
+// }
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    
-    return (
-        <div>
-            Hello World!
+class App extends React.Component {
+    render() {
+                    window.navigator.geolocation.getCurrentPosition(
+                    (position) => console.log(position),
+                    (err) => console.log(err)
+                );
+
+        return <div>
+            Latitude: 
             <SeasonDisplay />
         </div>
-    )
+    }
 }
 
 export default App;
