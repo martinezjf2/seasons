8004c732bf21af1902e086ba2e662034280e7d0a
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-23 20:34:56 -0500

    added this.state to obtain an object of latitude

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import SeasonDisplay from "./SeasonDisplay";
 class App extends React.Component {
     constructor(props) {
         super(props)
+        // what is super?  using the Constructor redefines our own props but we still want functions from the Constuctor parent. we call super in order to call the functions that "Component" brings.
+
+        this.state = {lat: null}
+        // the value of null means we dont know the number of the latitude yet, but we will know eventually
     }
 
     render() {
