ee0ee78f341869566419a85c2e84c53d639ae2ca
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-23 21:15:11 -0500

    added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -31,11 +31,19 @@ class App extends React.Component {
     }
 
     render() {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat} </div>
+        }
         
 
         return <div>
-            Latitude: {this.state.lat} <br/>
-            Error: {this.state.errorMessage}
+           Loading...
+            
             <SeasonDisplay />
         </div>
     }
