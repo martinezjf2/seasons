32c367c2cc260c6121776549bf703605de2c44ca
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-24 20:20:14 -0500

    initalized state through alternate path

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,17 @@ import SeasonDisplay from "./SeasonDisplay";
 // }
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        // what is super?  using the Constructor redefines our own props but we still want functions from the Constuctor parent. we call super in order to call the functions that "Component" brings.
+    // constructor(props) {
+    //     super(props)
+    //     // what is super?  using the Constructor redefines our own props but we still want functions from the Constuctor parent. we call super in order to call the functions that "Component" brings.
 
-        // the only time we do assignments
-        this.state = {lat: null, errorMessage: ''} 
-        // the value of null means we dont know the number of the latitude yet, but we will know some time in the future after we get our current position
-        
-    }
+    //     // the only time we do assignments
+    //     this.state = {lat: null, errorMessage: ''} 
+    //     // the value of null means we dont know the number of the latitude yet, but we will know some time in the future after we get our current position
+    // }
+
+    // Alternate initalization of state. This is equivalent using the constructor and initializating the state.
+    state = { lat: null, errorMessage: ''}
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
